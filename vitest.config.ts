27f25b3e91871e898283b765ff96c59f84b85c5f
Oliver Harris <36100915+revilo1882@users.noreply.github.com>
2025-07-12 11:20:05 +0100

    Remove duplicate vite config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
 export default defineConfig({
-	plugins: [tsconfigPaths(), react()],
+	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'happy-dom',
